ff501237857a55b4ed409c7f580103a862c53578
jaimeemi <emiliodelpin@gmail.com>
2023-11-16 00:24:33 -0300

    Nueva frmConsultaFunciones

---
+++ Cine_App_2/Formularios/FormsConsultas/frmConsultaFunciones.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Cine_App_2.Formularios.FormsConsultas
+{
+    partial class frmConsultaFunciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvResultados = new System.Windows.Forms.DataGridView();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblFechaHasta = new System.Windows.Forms.Label();
+            this.lblFechaDesde = new System.Windows.Forms.Label();
+            this.dtpHasta = new System.Windows.Forms.DateTimePicker();
+            this.dtpDesde = new System.Windows.Forms.DateTimePicker();
+            this.LblFunciones = new System.Windows.Forms.Label();
+            this.cboFunciones = new System.Windows.Forms.ComboBox();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.lblGenero = new System.Windows.Forms.Label();
+            this.chSubtitulada = new System.Windows.Forms.CheckBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.txtPelicula = new System.Windows.Forms.TextBox();
+            this.lblPelicula = new System.Windows.Forms.Label();
+            this.cbGenero = new System.Windows.Forms.ComboBox();
+            this.btnNuevaPeliucla = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvResultados
+            // 
+            this.dgvResultados.AllowUserToAddRows = false;
+            this.dgvResultados.AllowUserToDeleteRows = false;
+            this.dgvResultados.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgvResultados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResultados.Location = new System.Drawing.Point(0, 98);
+            this.dgvResultados.Name = "dgvResultados";
+            this.dgvResultados.ReadOnly = true;
+            this.dgvResultados.RowHeadersWidth = 51;
+            this.dgvResultados.Size = new System.Drawing.Size(795, 299);
+            this.dgvResultados.TabIndex = 4;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.BackColor = System.Drawing.Color.Gray;
+            this.btnCerrar.Font = new System.Drawing.Font("Liberation Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrar.Location = new System.Drawing.Point(670, 402);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(118, 45);
+            this.btnCerrar.TabIndex = 3;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = false;
+            this.btnCerrar.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Arial", 10.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.lblTitulo.Location = new System.Drawing.Point(42, 76);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(0, 16);
+            this.lblTitulo.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnNuevaPeliucla);
+            this.panel1.Controls.Add(this.cbGenero);
+            this.panel1.Controls.Add(this.lblPelicula);
+            this.panel1.Controls.Add(this.txtPelicula);
+            this.panel1.Controls.Add(this.chSubtitulada);
+            this.panel1.Controls.Add(this.lblGenero);
+            this.panel1.Controls.Add(this.btnConsultar);
+            this.panel1.Controls.Add(this.lblFechaHasta);
+            this.panel1.Controls.Add(this.lblFechaDesde);
+            this.panel1.Controls.Add(this.dtpHasta);
+            this.panel1.Controls.Add(this.dtpDesde);
+            this.panel1.Controls.Add(this.LblFunciones);
+            this.panel1.Controls.Add(this.cboFunciones);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(795, 92);
+            this.panel1.TabIndex = 15;
+            // 
+            // lblFechaHasta
+            // 
+            this.lblFechaHasta.AutoSize = true;
+            this.lblFechaHasta.Location = new System.Drawing.Point(215, 46);
+            this.lblFechaHasta.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblFechaHasta.Name = "lblFechaHasta";
+            this.lblFechaHasta.Size = new System.Drawing.Size(68, 13);
+            this.lblFechaHasta.TabIndex = 19;
+            this.lblFechaHasta.Text = "Fecha Hasta";
+            // 
+            // lblFechaDesde
+            // 
+            this.lblFechaDesde.AutoSize = true;
+            this.lblFechaDesde.Location = new System.Drawing.Point(6, 46);
+            this.lblFechaDesde.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblFechaDesde.Name = "lblFechaDesde";
+            this.lblFechaDesde.Size = new System.Drawing.Size(69, 13);
+            this.lblFechaDesde.TabIndex = 18;
+            this.lblFechaDesde.Text = "Fecha desde";
+            // 
+            // dtpHasta
+            // 
+            this.dtpHasta.Location = new System.Drawing.Point(218, 61);
+            this.dtpHasta.Margin = new System.Windows.Forms.Padding(2);
+            this.dtpHasta.Name = "dtpHasta";
+            this.dtpHasta.Size = new System.Drawing.Size(202, 20);
+            this.dtpHasta.TabIndex = 4;
+            // 
+            // dtpDesde
+            // 
+            this.dtpDesde.Location = new System.Drawing.Point(9, 61);
+            this.dtpDesde.Margin = new System.Windows.Forms.Padding(2);
+            this.dtpDesde.Name = "dtpDesde";
+            this.dtpDesde.Size = new System.Drawing.Size(202, 20);
+            this.dtpDesde.TabIndex = 3;
+            // 
+            // LblFunciones
+            // 
+            this.LblFunciones.AutoSize = true;
+            this.LblFunciones.Location = new System.Drawing.Point(338, 3);
+            this.LblFunciones.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LblFunciones.Name = "LblFunciones";
+            this.LblFunciones.Size = new System.Drawing.Size(45, 13);
+            this.LblFunciones.TabIndex = 15;
+            this.LblFunciones.Text = "Función";
+            // 
+            // cboFunciones
+            // 
+            this.cboFunciones.FormattingEnabled = true;
+            this.cboFunciones.Location = new System.Drawing.Point(341, 18);
+            this.cboFunciones.Margin = new System.Windows.Forms.Padding(2);
+            this.cboFunciones.Name = "cboFunciones";
+            this.cboFunciones.Size = new System.Drawing.Size(202, 21);
+            this.cboFunciones.TabIndex = 1;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnConsultar.Font = new System.Drawing.Font("Source Sans Pro", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsultar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnConsultar.Location = new System.Drawing.Point(670, 12);
+            this.btnConsultar.Margin = new System.Windows.Forms.Padding(0);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(118, 69);
+            this.btnConsultar.TabIndex = 7;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = false;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // lblGenero
+            // 
+            this.lblGenero.AutoSize = true;
+            this.lblGenero.Location = new System.Drawing.Point(545, 3);
+            this.lblGenero.Name = "lblGenero";
+            this.lblGenero.Size = new System.Drawing.Size(45, 13);
+            this.lblGenero.TabIndex = 21;
+            this.lblGenero.Text = "Genero:";
+            // 
+            // chSubtitulada
+            // 
+            this.chSubtitulada.AutoSize = true;
+            this.chSubtitulada.Location = new System.Drawing.Point(425, 64);
+            this.chSubtitulada.Name = "chSubtitulada";
+            this.chSubtitulada.Size = new System.Drawing.Size(79, 17);
+            this.chSubtitulada.TabIndex = 5;
+            this.chSubtitulada.Text = "Subtitulada";
+            this.chSubtitulada.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(9, 403);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(130, 45);
+            this.btnAgregar.TabIndex = 16;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnModificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificar.Location = new System.Drawing.Point(145, 403);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(130, 45);
+            this.btnModificar.TabIndex = 17;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = false;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.Red;
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(281, 402);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(130, 45);
+            this.btnEliminar.TabIndex = 18;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // txtPelicula
+            // 
+            this.txtPelicula.Location = new System.Drawing.Point(9, 18);
+            this.txtPelicula.Name = "txtPelicula";
+            this.txtPelicula.Size = new System.Drawing.Size(297, 20);
+            this.txtPelicula.TabIndex = 0;
+            // 
+            // lblPelicula
+            // 
+            this.lblPelicula.AutoSize = true;
+            this.lblPelicula.Location = new System.Drawing.Point(6, 3);
+            this.lblPelicula.Name = "lblPelicula";
+            this.lblPelicula.Size = new System.Drawing.Size(44, 13);
+            this.lblPelicula.TabIndex = 25;
+            this.lblPelicula.Text = "Pelicula";
+            // 
+            // cbGenero
+            // 
+            this.cbGenero.FormattingEnabled = true;
+            this.cbGenero.Location = new System.Drawing.Point(547, 18);
+            this.cbGenero.Margin = new System.Windows.Forms.Padding(2);
+            this.cbGenero.Name = "cbGenero";
+            this.cbGenero.Size = new System.Drawing.Size(121, 21);
+            this.cbGenero.TabIndex = 26;
+            // 
+            // btnNuevaPeliucla
+            // 
+            this.btnNuevaPeliucla.Font = new System.Drawing.Font("Alef", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNuevaPeliucla.Location = new System.Drawing.Point(312, 16);
+            this.btnNuevaPeliucla.Name = "btnNuevaPeliucla";
+            this.btnNuevaPeliucla.Size = new System.Drawing.Size(24, 23);
+            this.btnNuevaPeliucla.TabIndex = 27;
+            this.btnNuevaPeliucla.Text = "+";
+            this.btnNuevaPeliucla.UseVisualStyleBackColor = true;
+            this.btnNuevaPeliucla.Click += new System.EventHandler(this.btnNuevaPeliucla_Click);
+            // 
+            // frmConsultaFunciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(795, 461);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.dgvResultados);
+            this.Controls.Add(this.btnCerrar);
+            this.MaximumSize = new System.Drawing.Size(811, 500);
+            this.MinimumSize = new System.Drawing.Size(811, 500);
+            this.Name = "frmConsultaFunciones";
+            this.Text = "ABM Funciones";
+            this.Load += new System.EventHandler(this.Form6_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvResultados;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Label lblFechaHasta;
+        private System.Windows.Forms.Label lblFechaDesde;
+        private System.Windows.Forms.DateTimePicker dtpHasta;
+        private System.Windows.Forms.DateTimePicker dtpDesde;
+        private System.Windows.Forms.Label LblFunciones;
+        private System.Windows.Forms.ComboBox cboFunciones;
+        private System.Windows.Forms.CheckBox chSubtitulada;
+        private System.Windows.Forms.Label lblGenero;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label lblPelicula;
+        private System.Windows.Forms.TextBox txtPelicula;
+        private System.Windows.Forms.ComboBox cbGenero;
+        private System.Windows.Forms.Button btnNuevaPeliucla;
+    }
+}
---
+++ Cine_App_2/Formularios/FormsConsultas/frmConsultaFunciones.cs
@@ -0,0 +1,186 @@
+﻿using Cine_App_2.Datos;
+using Cine_App_2.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace Cine_App_2.Formularios.FormsConsultas
+{
+    public partial class frmConsultaFunciones : Form
+    {
+        List<Parametros> parametros = new List<Parametros>();
+        public string descripcion;
+        public string nombreSp;
+        
+        public frmConsultaFunciones()
+        {
+            InitializeComponent();
+            obtenerCombos();
+        }
+
+        private void Form6_Load(object sender, EventArgs e)
+        {
+            lblTitulo.Text = descripcion;
+            dtpHasta.Value = DateTime.Today;
+
+        }
+        private void obtenerCombos()
+        {
+            obtenerFunciones();
+            obtenerGeneros();
+        }
+
+        private void obtenerGeneros()
+        {
+            cbGenero.DataSource = ConsultasData.ejecutarConsulta("Select * From Generos");
+            cbGenero.Items.Insert(0, "ELEJIR GENERO"); 
+            cbGenero.ValueMember = "COD_GENERO";
+            cbGenero.DisplayMember = "NOMBRE";
+        }
+
+        private void obtenerFunciones()
+        {   
+           cboFunciones.DataSource = ConsultasData.ejecutarSpParams("PA_OBT_FUNCIONES");
+           cboFunciones.Items.Insert(0, "");
+           cboFunciones.ValueMember = "COD_FUNCION";
+           cboFunciones.DisplayMember = "NOMBRE";
+        }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            parametros.Add(new Parametros("@funcion", (((DataRowView)cboFunciones.SelectedValue).Row.ItemArray[0].ToString())));
+            parametros.Add(new Parametros("@desde"  , dtpDesde.Value.ToString()));
+            parametros.Add(new Parametros("@hasta"  , dtpHasta.Value.ToString()));
+
+            DataTable Resultado = ConsultasData.ejecutarSpParams("PA_REPORTE_ENTRADAS", parametros);
+            if (Resultado.Rows.Count > 0)
+            {
+                if (!Resultado.Rows[0].ItemArray[0].ToString().Contains("La fecha desde no puede ser mayor a la fecha hasta"))
+                {
+                    dgvResultados.DataSource = Resultado;
+                }
+                else
+                {
+                    MessageBox.Show("La fecha desde no puede ser mayor ni igual a la fecha hasta");
+                }
+            }
+            else
+            {
+                MessageBox.Show("No hay resultados");
+            }
+ 
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (txtPelicula.Text == "")
+            {
+                MessageBox.Show("El Campo Genero No puede quedar vacio");
+                txtPelicula.Focus();
+            }
+            if (cboFunciones.SelectedIndex == 0)
+            {
+                MessageBox.Show("Debe asignar funciones apropiada");
+                cboFunciones.Focus();
+            }
+            if (cbGenero.SelectedIndex == 0)
+            {
+                MessageBox.Show("Debe indicar el genero de la pelicula");
+                cbGenero.Focus();  
+            }
+            try
+            {
+                prNuevaFuncion();
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void prNuevaFuncion()
+        {
+            parametros.Add(new Parametros("@IDPelicula",txtPelicula.Text ));
+            parametros.Add(new Parametros("@IDSala", (((DataRowView)cbGenero.SelectedValue).Row.ItemArray[0].ToString())));
+           try
+            {
+
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            frmNuevaPelicula np = new frmNuevaPelicula();
+            np.Text = "Modificar Pelicula";
+            np.StartPosition = FormStartPosition.CenterScreen;
+            np.Show();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            DataGridViewCell f = dgvResultados.CurrentCell = this.dgvResultados[1, 0];
+            f = dgvResultados[1, 0];
+            /*
+             * bool resultado = MessageBox.Show("Confirma eliminar el registro: "+ f,
+                                            "Confirmacion de Eliminacion",
+                                            MessageBoxButtons.YesNo,
+                                            MessageBoxIcon.Question
+                                            );
+            */
+            DialogResult dialogResult = MessageBox.Show("Confirma eliminar el registro: " + f.ToString(),
+                                                         "Confirmacion eliminacion",
+                                                         MessageBoxButtons.YesNo,
+                                                         MessageBoxIcon.Question);
+            //bool resultado = ((bool)dialogResult);
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            string stQuery = "Select ";
+            string stNExo = " Where ";
+
+            stQuery += dtpDesde.Value.ToString() + dtpHasta.Value.ToString();
+            if (txtPelicula.Text != "")
+            {
+                stQuery += txtPelicula.Text;
+                stNExo += " AND ";
+            }
+            if (cboFunciones.SelectedIndex != 0)
+            {
+                stQuery += cboFunciones.Text;
+                stNExo += " AND ";
+            }
+            if (cbGenero.SelectedIndex != 0)
+                stQuery += cbGenero.Text;
+            try
+            {
+                dgvResultados.DataSource = ConsultasData.ejecutarConsulta(stQuery, false);
+            }
+            catch 
+            {
+                MessageBox.Show("Ocurrio un Error durante la carga de datos");
+            }
+        }
+
+        private void btnNuevaPeliucla_Click(object sender, EventArgs e)
+        {
+            frmNuevaPelicula np = new frmNuevaPelicula();
+            np.StartPosition = FormStartPosition.CenterParent;
+            np.ShowDialog();
+        }
+    }
+}
