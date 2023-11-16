69606d256d399d2bc40d15523483697958621df5
jaimeemi <emiliodelpin@gmail.com>
2023-11-16 00:23:54 -0300

    Entidad GenerosDAO y Eliminacion de ConsultasFunciones

---
+++ Cine_App_2/Entidades/GenerosDAO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Cine_App_2.Entidades
+{
+    internal class GenerosDAO
+    {
+        private int CODGENERO {  get; set; }
+        private string Nombre { get; set; }
+
+        public GenerosDAO(int codigo, string nombre)
+        { 
+            CODGENERO = codigo;
+            Nombre = nombre;
+        }
+    }
+}
